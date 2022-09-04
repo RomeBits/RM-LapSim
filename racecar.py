5da07997d33276e71fa1e6925e1749a86a5c0363
Benjamin Rome <benjamin.rome10@gmail.com>
2022-09-04 13:06:41 -0400

    updated to include engine model

--- racecar.py
+++ racecar.py
@@ -1,8 +1,20 @@
+import numpy as np
+
+
 class Car:
-    def __init__(self, file):
-        f = open(file)
+    def __init__(self, car_data, engine_data):
+        f = open(car_data, "r")
         self.params = {}
         
         for i in f:
             self.params[i.split()[0]] = i.split()[1]
+        f.close()
 
+        self.params['total_mass'] = self.params['driver_mass'] + self.params['car_mass']
+
+        f = open(engine_data, "r")
+        self.engine = np.genfromtxt(f, delimiter=',')
+        f.close()
+
+    def calc_forward_step(self):
+        pass
